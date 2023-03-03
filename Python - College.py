ca21b644fcd765fadb4392e065c9b1a04421dd4a
Sirsha Dey <69218962+Sirsha2468@users.noreply.github.com>
2023-03-03 12:10:56 +0530

    Create Python - College.py

---
+++ Python - College.py
@@ -0,0 +1,45 @@
+# # Program to check whether the square root of a number is prime or not
+# # from math import sqrt
+# # prime = eval(input("Enter the number:"));
+
+# # number = 0;
+
+# # if(prime > 1):
+# #     for i in range(2, int(sqrt(prime)) + 1):
+# #         if(prime % i == 0):
+# #             number = 1;
+# #         break;
+# #     if(number == 0):
+# #         print(prime, "is a prime number");
+# #     else:
+# #         print(prime, "is not a prime number");
+# # else:
+# #     print(prime, "is not a prime number");
+
+# # length of a number
+# # number = 15327;
+# # print(len(str(number)));
+
+# # counter = 1
+# # n = int(input("Enter a number for checking the length: "));
+# # num = 15832
+# # c = 1
+# # for c in "n//c": # Travarse the number 15327
+# #     counter+=1 # increment the counter
+# # print ("The length of",n, "is", counter) # outputs the length (5) of the number 15327
+# def factorial(n):
+#     fact = 1;
+#     for i in range(2, n + 1):
+#         fact = fact * i;
+#     return fact
+    
+# def sum(n):
+#     s = 0.0
+     
+#     for i in range(1, n + 1):
+#         s += 1.0 / factorial(i)
+#     print(s)
+ 
+# # Driver program to test above functions
+# n = 5
+# sum(n)
